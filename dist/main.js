391d1d93b1b2a1973de7a9c194ac0ee54e4aba2e
Nova988 <antonionovak1998@hotmail.com>
2022-03-22 22:33:27 +0100

    added responsivness for medium, large and extra large screens

--- dist/main.js
+++ dist/main.js
@@ -3,6 +3,14 @@
 const hamburgerBtn = document.getElementById("hamburger_btn");
 const hamburgerOverlay = document.querySelector(".hamburger__menu__overlay");
 const hamburgerEl = document.querySelectorAll(".hamburger-el");
+const images = document.querySelectorAll(".image");
+
+// Media
+const mediaMedium = window.matchMedia("(min-width: 600px)");
+
+if (mediaMedium.matches) {
+  images.forEach((img) => (img.src = img.src.replace("mobile", "desktop")));
+}
 
 const toggleHide = function () {
   hamburgerOverlay.classList.toggle("hide");
